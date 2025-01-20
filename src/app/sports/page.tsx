229e3e897b563d8a15dfe092a5c6dcb5cf13c6cd
Yusup <yusuprd@icloud.com>
2025-01-20 13:41:25 +0500

    resolve conflict for deploy

---
+++ src/app/sports/page.tsx
@@ -0,0 +1,19 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Online Casino Games - Slots, Classic Casino Games & Bonuses",
+  description:
+    "Discover an exciting selection of online casino games at FaFa878. Play slots, table games, and live dealer games while enjoying unbeatable bonuses and thrilling gameplay. Start your casino adventure today!",
+};
+
+const SportsPage = () => {
+  return (
+    <div>
+      <div>
+        Sports Page
+      </div>
+    </div>
+  );
+};
+
+export default SportsPage;
