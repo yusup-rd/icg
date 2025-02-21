9df3ac2024c254dd3ff71e412b994e474ff30b92
Yusup <yusuprd@icloud.com>
2025-02-21 11:49:59 +0500

    Added new flags that will be used for translations in future

--- src/utils/flagsUtil.ts
+++ src/utils/flagsUtil.ts
@@ -1,6 +1,14 @@
 const flags: Record<string, string> = {
-  en: "/flags/en.svg",
-  fr: "/flags/fr.svg",
+  en: "/flags/en.svg", // English
+  fr: "/flags/fr.svg", // French
+  zh: "/flags/zh.svg", // Chinese
+  ms: "/flags/ms.svg", // Malay
+  hi: "/flags/hi.svg", // Hindi
+  vi: "/flags/vi.svg", // Vietnamese
+  ur: "/flags/ur.svg", // Urdu
+  bn: "/flags/bn.svg", // Bengali
+  th: "/flags/th.svg", // Thai
+  my: "/flags/my.svg", // Burmese
 };
 
 export default flags;
