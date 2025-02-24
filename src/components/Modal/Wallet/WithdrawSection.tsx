fe5f9653565e3e60acb762c74d0a67c3ec07d29e
Yusup <yusuprd@icloud.com>
2025-02-24 19:08:16 +0500

    Added available balance dropdown in withdraw section of wallet modal

--- src/components/Modal/Wallet/WithdrawSection.tsx
+++ src/components/Modal/Wallet/WithdrawSection.tsx
@@ -6,12 +6,14 @@ import { useTranslations } from "next-intl";
 const WithdrawSection = () => {
   const t = useTranslations("Modal.Wallet.Withdraw");
 
+  const [selectedBalance, setSelectedBalance] = useState(currencies[0]);
   const [selectedCurrency, setSelectedCurrency] = useState(currencies[0]);
   const [selectedBlockchain, setSelectedBlockchain] = useState(
     selectedCurrency.blockchains.length > 0
       ? selectedCurrency.blockchains[0]
       : null,
   );
+  const [isBalanceDropdownOpen, setIsBalanceDropdownOpen] = useState(false);
   const [isCurrencyDropdownOpen, setIsCurrencyDropdownOpen] = useState(false);
   const [isBlockchainDropdownOpen, setIsBlockchainDropdownOpen] =
     useState(false);
@@ -42,6 +44,9 @@ const WithdrawSection = () => {
     if (isBlockchainDropdownOpen) {
       setIsBlockchainDropdownOpen(false);
     }
+    if (isBalanceDropdownOpen) {
+      setIsBalanceDropdownOpen(false);
+    }
   };
 
   const handleBlockchainClick = () => {
@@ -49,10 +54,79 @@ const WithdrawSection = () => {
     if (isCurrencyDropdownOpen) {
       setIsCurrencyDropdownOpen(false);
     }
+    if (isBalanceDropdownOpen) {
+      setIsBalanceDropdownOpen(false);
+    }
+  };
+
+  const handleBalanceClick = () => {
+    setIsBalanceDropdownOpen((prev) => !prev);
+    if (isCurrencyDropdownOpen) {
+      setIsCurrencyDropdownOpen(false);
+    }
+    if (isBlockchainDropdownOpen) {
+      setIsBlockchainDropdownOpen(false);
+    }
   };
 
   return (
-    <div className="rounded bg-card p-4">
+    <div className="space-y-3 rounded bg-card p-4">
+      <div className="flex flex-col gap-1">
+        <label className="text-sm">{t("availableBalanceLabel")}</label>
+        <div className="relative" ref={dropdownRef}>
+          {/* Custom Available Balance Dropdown */}
+          <div
+            className="flex min-h-14 cursor-pointer items-center justify-between gap-6 rounded bg-white px-3 py-2 shadow-md"
+            onClick={handleBalanceClick}
+          >
+            <span className="flex w-full items-center gap-2 text-nowrap">
+              <div className="flex w-full items-center justify-between gap-3">
+                <span className="flex-1 truncate text-sm font-semibold">
+                  0.00000000
+                </span>
+                <div>
+                  <selectedBalance.Icon
+                    className="size-full"
+                    style={{ color: selectedBalance.color }}
+                  />
+                </div>
+              </div>
+            </span>
+            <div>
+              {isBalanceDropdownOpen ? (
+                <FaChevronDown className="rotate-180 duration-100" />
+              ) : (
+                <FaChevronDown className="rotate-0 duration-100" />
+              )}
+            </div>
+          </div>
+
+          {isBalanceDropdownOpen && (
+            <ul className="absolute z-10 mt-1 w-full divide-y rounded bg-white py-2 shadow-md">
+              {currencies.map((item) => (
+                <li
+                  key={item.name}
+                  onClick={() => {
+                    setSelectedBalance(item);
+                    setIsBalanceDropdownOpen(false);
+                    setIsCurrencyDropdownOpen(false);
+                    setIsBlockchainDropdownOpen(false);
+                  }}
+                  className="cursor-pointer px-3 py-2 hover:bg-gray-100"
+                >
+                  <div className="flex items-center justify-between gap-2">
+                    <span className="py-4 text-xs opacity-80">0.00000000</span>
+                    <div>
+                      <item.Icon style={{ color: item.color }} />
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
+
       <div className="flex flex-col gap-1">
         <label className="text-sm">{t("currencyLabel")}</label>
         <div className="relative" ref={dropdownRef}>
@@ -87,12 +161,13 @@ const WithdrawSection = () => {
           </div>
 
           {isCurrencyDropdownOpen && (
-            <ul className="absolute z-10 mt-1 w-full rounded bg-white py-2 shadow-md">
+            <ul className="absolute z-10 mt-1 w-full divide-y rounded bg-white py-2 shadow-md">
               {currencies.map((item) => (
                 <li
                   key={item.name}
                   onClick={() => {
                     setSelectedCurrency(item);
+                    setSelectedBalance(item);
                     setSelectedBlockchain(
                       item.blockchains.length > 0 ? item.blockchains[0] : null,
                     );
@@ -123,7 +198,7 @@ const WithdrawSection = () => {
       </div>
 
       {selectedCurrency.blockchains.length > 0 && (
-        <div className="mt-3 flex flex-col gap-1">
+        <div className="flex flex-col gap-1">
           <label className="text-sm">{t("networkLabel")}</label>
           <div className="relative">
             {/* Custom Blockchain Network Dropdown */}
@@ -144,7 +219,7 @@ const WithdrawSection = () => {
             </div>
 
             {isBlockchainDropdownOpen && (
-              <ul className="absolute z-10 mt-1 w-full rounded bg-white py-2 shadow-md">
+              <ul className="absolute z-10 mt-1 w-full divide-y rounded bg-white py-2 shadow-md">
                 {selectedCurrency.blockchains.map((blockchain) => (
                   <li
                     key={blockchain.networkName}
@@ -166,7 +241,7 @@ const WithdrawSection = () => {
         </div>
       )}
 
-      <div className="mt-3 flex flex-col gap-1">
+      <div className="flex flex-col gap-1">
         <label className="text-sm">
           {t("addressLabel", { currency: selectedCurrency.name })}
         </label>
@@ -176,7 +251,7 @@ const WithdrawSection = () => {
         />
       </div>
 
-      <div className="mt-3 flex flex-col gap-1">
+      <div className="flex flex-col gap-1">
         <label className="text-sm">{t("amountLabel")}</label>
         <div className="flex justify-between gap-2">
           <div className="group relative flex flex-1 items-center">
@@ -197,7 +272,7 @@ const WithdrawSection = () => {
         </div>
       </div>
 
-      <div className="mt-6">
+      <div className="my-12">
         <button className="min-h-14 w-full rounded bg-primary px-3 py-2 font-semibold text-white shadow-md duration-200 hover:bg-secondary">
           {t("withdraw")}
         </button>
