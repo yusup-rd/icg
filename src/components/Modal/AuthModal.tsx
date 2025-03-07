9adc0ec10d2f8caa36a7c83694795a5c2dc2c19a
Yusup <yusuprd@icloud.com>
2025-03-07 14:39:04 +0500

    Fixed: 'Skipping auto scroll behaviiour' error

--- src/components/Modal/AuthModal.tsx
+++ src/components/Modal/AuthModal.tsx
@@ -15,40 +15,42 @@ const AuthModal = ({
 
   return (
     <AnimatePresence>
-      <motion.div
-        className="fixed inset-0 z-50 flex items-center justify-center md:bg-black/80"
-        key="overlay"
-        {...motionVariants}
-        variants={overlayMotion}
-      >
+      <div>
         <motion.div
-          className="relative flex h-full w-full flex-col bg-white shadow-lg md:max-h-[90vh] md:w-8/12 md:rounded-lg lg:w-6/12"
-          key="modal"
+          className="fixed inset-0 z-50 flex items-center justify-center md:bg-black/80"
+          key="overlay"
           {...motionVariants}
-          variants={modalMotion}
+          variants={overlayMotion}
         >
-          <div className="flex items-center justify-between p-4">
-            <Image
-              src="/logo.png"
-              alt="Logo"
-              width={0}
-              height={0}
-              sizes="100vw"
-              priority
-              className="h-10 w-auto"
-            />
-            <button
-              className="text-gray-500 duration-200 hover:text-gray-800"
-              onClick={() => router.back()}
-            >
-              <FaXmark className="size-5" />
-            </button>
-          </div>
-          <div className="w-full border ring-gray-500"></div>
+          <motion.div
+            className="relative flex h-full w-full flex-col bg-white shadow-lg md:max-h-[90vh] md:w-8/12 md:rounded-lg lg:w-6/12"
+            key="modal"
+            {...motionVariants}
+            variants={modalMotion}
+          >
+            <div className="flex items-center justify-between p-4">
+              <Image
+                src="/logo.png"
+                alt="Logo"
+                width={0}
+                height={0}
+                sizes="100vw"
+                priority
+                className="h-10 w-auto"
+              />
+              <button
+                className="text-gray-500 duration-200 hover:text-gray-800"
+                onClick={() => router.back()}
+              >
+                <FaXmark className="size-5" />
+              </button>
+            </div>
+            <div className="w-full border ring-gray-500"></div>
 
-          {children}
+            {children}
+          </motion.div>
         </motion.div>
-      </motion.div>
+      </div>
     </AnimatePresence>
   );
 };
